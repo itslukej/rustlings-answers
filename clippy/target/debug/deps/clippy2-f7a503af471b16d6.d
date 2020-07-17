377d7a511e25b63a3100234bdc67b0ea2ab91b43
Luke James <me@lukej.me>
2020-07-17 16:14:28 +0100

    Add answers

---
+++ clippy/target/debug/deps/clippy2-f7a503af471b16d6.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/Luke/Documents/Development/rustlings/exercises/clippy/target/debug/deps/clippy2-f7a503af471b16d6.rmeta: clippy2.rs
+
+/mnt/c/Users/Luke/Documents/Development/rustlings/exercises/clippy/target/debug/deps/clippy2-f7a503af471b16d6.d: clippy2.rs
+
+clippy2.rs:
